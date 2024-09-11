2f8c8f48e03b1f9f4fff056d4d979d7e638dd88b
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-09-11 13:28:44 -0300

    Add CMake server project

---
+++ DPLMPServer/DPLMPServer.cpp
@@ -0,0 +1,12 @@
+﻿// DPLMPServer.cpp : Defines the entry point for the application.
+//
+
+#include "DPLMPServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
